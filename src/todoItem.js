6388f94f22473809e8731843dda84adc7b99f6d3
raych2 <rtongco@gmail.com>
2021-01-20 23:17:58 -0800

    Create Todo class

---
+++ src/todoItem.js
@@ -0,0 +1,14 @@
+class Todo {
+    constructor (title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
+
+//default todo list
+const myTodos = [];
+
+
+export {myTodos, Todo};
